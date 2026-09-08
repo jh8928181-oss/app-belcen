1f9d3f713f9bafdd49009e6be226da4b2445d39b
jeanpierre <jh8928181@gmail.com>
2026-09-08 14:23:31 -0500

    Corregir conexion de base de datos para produccion

--- db.js
+++ db.js
@@ -1,16 +1,12 @@
 const { Pool } = require('pg');
 
-// Configuración de credenciales de PostgreSQL
 const pool = new Pool({
-  user: 'postgres',          // Tu usuario de PostgreSQL
-  host: 'localhost',         // O la IP del NAS en red local
-  database: 'belcen_db',     // Nombre de la base de datos
-  password: '123456',    // Tu contraseña de PostgreSQL
-  port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 pool.on('connect', () => {
-  console.log('Conectado exitosamente a la Base de Datos PostgreSQL');
+    console.log('Conectado exitosamente a la Base de Datos PostgreSQL');
 });
 
 module.exports = pool;
